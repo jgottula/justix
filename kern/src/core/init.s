7fc1a7350df74f5338fd589fddfe82d0a456d0fe
Justin Gottula <justin@jgottula.com>
2013-01-31 21:12:17 -0800

    update init code

--- kern/src/core/init.s
+++ kern/src/core/init.s
@@ -3,6 +3,7 @@
 %include 'core/gdt.inc'
 %include 'core/idt.inc'
 %include 'lib/debug.inc'
+%include 'serial/serial.inc'
 %include 'video/video.inc'
 	
 	section .text.init
@@ -26,99 +27,36 @@ kern_entry:
 	
 	call idt_setup
 	
-	int 0x0d
-	int 0x0d
+	invoke serial_detect
+	invoke serial_init,0,SER_38400,SER_8N1
 	
-	call video_clear_screen
+.infinite:
+	invoke serial_send_str,0,kern_hello
 	
-	invoke debug_stack_trace,0xaa,0xbb,0xcc
+	; this stuff all needs to GO
+	invoke video_clear_screen
+	mov word [0xb9000],0x7000|'J'
+	mov word [0xb9002],0x7000|'G'
+	mov word [0xb9004],0x7000|'S'
+	mov word [0xb9006],0x7000|'Y'
+	mov word [0xb9008],0x7000|'S'
+	mov word [0xb900a],0x7000|' '
+	mov word [0xb900c],0x7000|'k'
+	mov word [0xb900e],0x7000|'e'
+	mov word [0xb9010],0x7000|'r'
+	mov word [0xb9012],0x7000|'n'
 	
-	;sti
+	jmp $
 	
-	; serial?
-;   outb(PORT + 1, 0x00);    // Disable all interrupts
-;   outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-;   outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-;   outb(PORT + 1, 0x00);    //                  (hi byte)
-;   outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-;   outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-;   outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-	mov dx,0x3f8+1
-	mov ax,0x00
-	out dx,ax
-	mov dx,0x3f8+1
-	mov ax,0x00
-	out dx,ax
-	mov dx,0x3f8+3
-	mov ax,0x80
-	out dx,ax
-	mov dx,0x3f8+0
-	mov ax,0x03
-	out dx,ax
-	mov dx,0x3f8+1
-	mov ax,0x00
-	out dx,ax
-	mov dx,0x3f8+3
-	mov ax,0x03
-	out dx,ax
-	mov dx,0x3f8+2
-	mov ax,0xc7
-	out dx,ax
-	mov dx,0x3f8+4
-	mov ax,0x0b
-	out dx,ax
 	
-	mov al,'e'
-	out 0xe9,al
-	mov al,'9'
-	out 0xe9,al
+	section .rodata
 	
-	mov dx,0x8a00
-	mov ax,0x8a00
-	out dx,ax
-	mov ax,0x8ae0
-	out dx,ax
-	
-	mov al,'H'
-	call serial_write_char
-	mov al,'i'
-	call serial_write_char
-	
-	mov word [0xb9000],0x7000|'H'
-	mov word [0xb9002],0x7000|'i'
-	
-	mov word [0x462],0x00
-	
-kern_stop:
-	cli
-	hlt
-	jmp kern_stop
-	
-serial_write_char:
-	push ax
-	push dx
-	
-	mov dx,0x3f8+5
-	
-.wait:
-	in al,dx
-	test al,0x20
-	jz .wait
-	
-	pop ax
-	
-	mov dx,0x3f8+0
-	out dx,al
-	
-	pop dx
-	
-	ret
+gdata kern_hello
+	db `JGSYS kern\r\n`
 	
 	
 	section .bss
 	
-	global kern_stack_top:data
-kern_stack_top:
+gdata kern_stack_top
 	resb 0x1000
-	global kern_stack_bottom:data
-kern_stack_bottom:
+gdata kern_stack_bottom
