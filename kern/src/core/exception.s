30c619c751a2024e04197690f77fb8bd760187ab
Justin Gottula <justin@jgottula.com>
2013-01-31 23:20:35 -0800

    add stack trace to gpf handler

--- kern/src/core/exception.s
+++ kern/src/core/exception.s
@@ -1,12 +1,27 @@
 %include 'common/header.inc'
 %include 'core/exception.inc'
+%include 'lib/debug.inc'
 	
 	section .text
 	
 	global trap_gpf:function
 trap_gpf:
-	mov word [0xb9006],0x7000|'G'
-	mov word [0xb9008],0x7000|'P'
-	mov word [0xb900a],0x7000|'F'
+	frame
+	pushad
 	
+	invoke debug_write_str,str_gpf
+	invoke debug_stack_trace,ebp
+	
+	mov word [0xb90a0],0x7000|'G'
+	mov word [0xb90a2],0x7000|'P'
+	mov word [0xb90a4],0x7000|'F'
+	
+	popad
+	unframe
 	iret
+	
+	
+	section .rodata
+	
+str_gpf:
+	strz `GPF!\n`
