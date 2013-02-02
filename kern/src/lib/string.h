b4eb658bad1fbe509ef1d0816ef0f80ff90a3245
Justin Gottula <justin@jgottula.com>
2013-02-02 01:59:51 -0800

    fix problematic pointer declarations

--- kern/src/lib/string.h
+++ kern/src/lib/string.h
@@ -5,10 +5,9 @@
 
 uint32_t strlen(const char *s);
 int32_t strcmp(const char *restrict s1, const char *restrict s2);
-uint8_t memcmp(const uint8_t *restrict m1, const uint8_t *restrict m2,
-	uint32_t size);
-void memset(const uint8_t *mem, uint8_t val, uint32_t size);
-void memcpy(uint8_t *restrict dst, const uint8_t *restrict src, uint32_t size);
+uint8_t memcmp(const void *restrict m1, const void *restrict m2, uint32_t size);
+void memset(void *mem, uint8_t val, uint32_t size);
+void memcpy(void *restrict dst, const void *restrict src, uint32_t size);
 
 #else
 
