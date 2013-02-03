cf4e141183a859b289c88c38ba765b63661a5ea4
Justin Gottula <justin@jgottula.com>
2013-02-03 03:18:52 -0800

    add debug_dump_{saved_reg,mem}

--- kern/src/lib/debug.c
+++ kern/src/lib/debug.c
@@ -97,3 +97,52 @@ void debug_write_dec(uint32_t dec) {
 		divisor /= 10;		
 	} while (divisor >= 1);
 }
+
+void debug_dump_saved_reg(uint32_t *regs) {
+	/* registers in ascending order in memory:
+	 * edi esi ebp esp ebx edx ecx eax eflags */
+	
+	debug_write_str("registers:\n");
+	
+	debug_write_fmt("eax    %xd\n", regs[7]);
+	debug_write_fmt("ebx    %xd\n", regs[4]);
+	debug_write_fmt("ecx    %xd\n", regs[6]);
+	debug_write_fmt("edx    %xd\n", regs[5]);
+	debug_write_fmt("esi    %xd\n", regs[1]);
+	debug_write_fmt("edi    %xd\n", regs[0]);
+	debug_write_fmt("ebp    %xd\n", regs[2]);
+	debug_write_fmt("esp    %xd\n", regs[3]);
+	debug_write_fmt("eflags %xd\n", regs[8]);
+}
+
+void debug_dump_mem(void *addr, uint32_t len) {
+	uintptr_t start = (uintptr_t)addr & ~0xf;
+	uintptr_t end   = (uintptr_t)((uint8_t *)addr + len);
+	
+	if ((end & 0xf) != 0) {
+		end &= 0xf;
+		end += 0x10;
+	}
+	
+	debug_write_fmt("mem dump @ %xd (len %xd):\n", addr, len);
+	
+	uint8_t *ptr = (uint8_t *)start;
+	uint32_t i   = 0;
+	
+	while (ptr != (uint8_t *)end) {
+		if (i == 0) {
+			debug_write_fmt("%xd: ", ptr);
+		} else if (i == 8) {
+			debug_write_chr(' ');
+		} else if (i == 15) {
+			debug_write_chr('\n');
+		}
+		
+		debug_write_fmt(" %xb", *ptr);
+		
+		++ptr;
+		if (++i >= 0x10) {
+			i = 0;
+		}
+	}
+}
--- kern/src/lib/debug.h
+++ kern/src/lib/debug.h
@@ -11,6 +11,8 @@ void debug_write_hex16(uint16_t hex);
 void debug_write_hex32(uint32_t hex);
 void debug_write_dec(uint32_t dec);
 void debug_stack_trace(void *base_ptr);
+void debug_dump_saved_reg(uint32_t *regs);
+void debug_dump_mem(void *addr, uint32_t len);
 
 #else
 
@@ -21,6 +23,8 @@ extern debug_write_hex8
 extern debug_write_hex16
 extern debug_write_hex32
 extern debug_write_dec
+extern debug_dump_saved_reg
+extern debug_dump_mem
 
 %ifndef jgsys_kern_lib_debug
 extern debug_stack_trace
