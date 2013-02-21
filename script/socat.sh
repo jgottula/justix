51cd26723600588d566cb98468586969bf23ca92
Justin Gottula <justin@jgottula.com>
2013-02-20 21:50:18 -0800

    make socat script one-instance-only

--- script/socat.sh
+++ script/socat.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sudo socat PTY,link=/dev/ttyBochs0,mode=0666,raw,crtscts=1,b115200 PTY,link=/dev/ttyBochs1,mode=666,raw,crtscts=1,b115200
+(
+	if ! flock -n 3; then
+		echo "$0: already running" >&2
+		exit 1
+	fi
+	
+	sudo socat PTY,link=/dev/ttyBochs0,mode=0666,raw,crtscts=1,b115200 \
+		PTY,link=/dev/ttyBochs1,mode=666,raw,crtscts=1,b115200
+) 3>/tmp/justix_socat.lock
